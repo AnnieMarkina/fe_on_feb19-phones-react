419f505771caf44060c8fbe7e359655135e9f29f
AnnieMarkina <anniemurkina@gmail.com>
2019-06-19 14:48:49 +0300

    Updates

--- precache-manifest.72e02a2367c7a4be1f850a7868074b5c.js
+++ precache-manifest.72e02a2367c7a4be1f850a7868074b5c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bccfd664c946a9206a44530c0d6c5f3",
+    "revision": "c9c1760f45a1aa496f6b727954e04bb8",
     "url": "/fe_on_feb19-phones-react/index.html"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
+    "revision": "02faf120bcaa75a0d1a8",
     "url": "/fe_on_feb19-phones-react/static/css/main.74074b87.chunk.css"
   },
   {
-    "revision": "681b51057d74cfa8975d",
-    "url": "/fe_on_feb19-phones-react/static/js/2.65aa1cca.chunk.js"
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/fe_on_feb19-phones-react/static/js/2.7e675ef3.chunk.js"
   },
   {
-    "revision": "a7c066b342f2c74f30bd",
-    "url": "/fe_on_feb19-phones-react/static/js/main.d9da4509.chunk.js"
+    "revision": "02faf120bcaa75a0d1a8",
+    "url": "/fe_on_feb19-phones-react/static/js/main.a2db66db.chunk.js"
   },
   {
     "revision": "13ef023f66a5d3f30d63",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fe_on_feb19-phones-react/precache-manifest.4321a3f769abe4bdae7dfdebc8fba4d9.js"
+  "/fe_on_feb19-phones-react/precache-manifest.72e02a2367c7a4be1f850a7868074b5c.js"
 );
 
 self.addEventListener('message', (event) => {
